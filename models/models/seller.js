87b1f0bee6703adc60a5220cbeea8856f5cbfc8e
Purvi Patel <91878359+purvi1999@users.noreply.github.com>
2021-12-02 18:07:36 +0530

    Delete models/models directory

--- models/models/seller.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const SellerSchema = mongoose.Schema({
-    seller_id: String,
-    name: String,
-    Product_ids: Array
-});
-const SellerModels = mongoose.model("DBSeller", SellerSchema, "DBSeller");
-module.exports = SellerModels;
